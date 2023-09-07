26a6da878ffbff585c24c26dbf31897da6a11671
anhson <sonanhnguyen003@gmail.com>
2023-09-08 04:58:33 +0700

    Week1

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.Surface
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.helloworld.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+    }
+
+
+}
