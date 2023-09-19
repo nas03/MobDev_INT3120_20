f6403784ae2b2cc3b748f6d0be04c370be2be061
anhson <sonanhnguyen003@gmail.com>
2023-09-19 08:13:00 +0700

    Week_5

---
+++ Week_5/app/src/main/java/com/example/week_5/DateAndTime.java
@@ -0,0 +1,64 @@
+package com.example.week_5;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class DateAndTime extends Activity {
+    DateFormat fmtDateAndTime = DateFormat.getDateTimeInstance();
+    TextView lblDateAndTime;
+    Calendar myCalendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+    TimePickerDialog.OnTimeSetListener t = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            myCalendar.set(Calendar.MINUTE, minute);
+            updateLabel();
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.date_and_time);
+        lblDateAndTime = (TextView) findViewById(R.id.lblDateAndTime);
+        Button btnDate = (Button) findViewById(R.id.btnDate);
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new DatePickerDialog(DateAndTime.this, d, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        Button btnTime = (Button) findViewById(R.id.btnTime);
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new TimePickerDialog(DateAndTime.this, t, myCalendar.get(Calendar.HOUR_OF_DAY), myCalendar.get(Calendar.MINUTE), true).show();
+            }
+        });
+        updateLabel();
+    }
+
+    private void updateLabel() {
+        lblDateAndTime.setText(fmtDateAndTime.format(myCalendar.getTime()));
+    }
+}
+
+
+
+
+
---
+++ Week_5/app/src/main/java/com/example/week_5/TabSelectorActivity.java
@@ -0,0 +1,47 @@
+package com.example.week_5;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TabHost;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class TabSelectorActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_tab);
+        TabHost tabs = findViewById(R.id.tabhost);
+        tabs.setup();
+        TabHost.TabSpec spec;
+        spec = tabs.newTabSpec("tag1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("1-Clock");
+        tabs.addTab(spec);
+        tabs.setCurrentTab(0);
+        spec = tabs.newTabSpec("tag2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("2-Login", ContextCompat.getDrawable(this, android.R.drawable.ic_menu_info_details));
+        tabs.addTab(spec);
+        Button btnGo = findViewById(R.id.btnGo);
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                EditText txtPerson = findViewById(R.id.txtPerson);
+                String theUser = txtPerson.getText().toString();
+                txtPerson.setText("Hola " + theUser);
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
